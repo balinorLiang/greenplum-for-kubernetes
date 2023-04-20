502c61cf0769e53ba7e347a88309654b6f02e58d
Adams Tower <adams.tower@tartansolutions.com>
2023-04-20 15:03:12 -0400

    Undo that for this branch

--- greenplum-operator/controllers/greenplumcluster/greenplumcluster_controller.go
+++ greenplum-operator/controllers/greenplumcluster/greenplumcluster_controller.go
@@ -174,20 +174,20 @@ func (r *GreenplumClusterReconciler) createOrUpdateClusterResources(ctx context.
 	}
 	r.logReconcileResult(operationResult, agentService)
 
-	// greenplumService := &corev1.Service{
-	// 	ObjectMeta: metav1.ObjectMeta{
-	// 		Name:      "greenplum",
-	// 		Namespace: ns,
-	// 	},
-	// }
-	// operationResult, err = ctrl.CreateOrUpdate(ctx, r, greenplumService, func() error {
-	// 	service.ModifyGreenplumService(gpName, greenplumService)
-	// 	return ctrl.SetControllerReference(&greenplumCluster, greenplumService, r.Scheme())
-	// })
-	// if err != nil {
-	// 	return err
-	// }
-	// r.logReconcileResult(operationResult, greenplumService)
+	greenplumService := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "greenplum",
+			Namespace: ns,
+		},
+	}
+	operationResult, err = ctrl.CreateOrUpdate(ctx, r, greenplumService, func() error {
+		service.ModifyGreenplumService(gpName, greenplumService)
+		return ctrl.SetControllerReference(&greenplumCluster, greenplumService, r.Scheme())
+	})
+	if err != nil {
+		return err
+	}
+	r.logReconcileResult(operationResult, greenplumService)
 
 	serviceAccount := &corev1.ServiceAccount{
 		ObjectMeta: metav1.ObjectMeta{
